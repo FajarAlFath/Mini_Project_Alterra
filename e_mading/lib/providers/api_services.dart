6da166c194eca3812a51af26a9939962dfbff744
Muhammad Fajar Al Fath <99872320+FajarAlFath@users.noreply.github.com>
2022-07-03 18:59:27 +0700

    Update api_services.dart
    revisi code

--- e_mading/lib/providers/api_services.dart
+++ e_mading/lib/providers/api_services.dart
@@ -3,34 +3,19 @@ import 'dart:convert';
 import 'package:http/http.dart';
 
 import '../model/news_model.dart';
-
-//Now let's make the HTTP request services
-// this class will alows us to make a simple get http request
-// from the API and get the Articles and then return a list of Articles
-
 class ApiService {
-  //let's add an Endpoint URL, you can check the website documentation
-  // and learn about the different Endpoint
-  //for this example I'm going to use a single endpoint
-
-  //NOTE: make sure to use your OWN apikey, you can make a free acount and
-  // choose a developer option it's FREE
   final endPointUrl = Uri.parse(
       "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=d096545346e9429ba1cb98050b7ba704");
-
-  //Now let's create the http request function
-  // but first let's import the http package
-
+  
   Future<List<Article>> getArticle() async {
     Response res = await get(endPointUrl);
 
-    //first of all let's check that we got a 200 statu code: this mean that the request was a succes
+  
     if (res.statusCode == 200) {
       Map<String, dynamic> json = jsonDecode(res.body);
 
       List<dynamic> body = json['articles'];
 
-      //this line will allow us to get the different articles from the json file and putting them into a list
       List<Article> articles =
           body.map((dynamic item) => Article.fromJson(item)).toList();
 
